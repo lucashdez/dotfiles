7377b93b9d6e3fef0c8bee5a6c82a9f5526a935d
lucashdez <lukastpiano@gmail.com>
2025-10-28 22:11:21 +0000

    feat(org-super-agenda)

--- emacs/config.el
+++ emacs/config.el
@@ -1,14 +1,14 @@
 (defvar bootstrap-version)
 (let ((bootstrap-file
-	   (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	  (bootstrap-version 6))
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
-	(with-current-buffer
-		(url-retrieve-synchronously
-		 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-		 'silent 'inhibit-cookies)
-	  (goto-char (point-max))
-	  (eval-print-last-sexp)))
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 ;; Integrar straight.el con use-package
 (straight-use-package 'use-package)
@@ -34,26 +34,22 @@
 (add-to-list 'default-frame-alist '(alpha-background . 90))
 (setq split-width-threshold 999999)
 
-(use-package doom-themes
-  :init (load-theme 'doom-wilmersdorf t))
-(use-package consult)
-(use-package company
-  :init
-  (global-company-mode 1)
-  (add-to-list 'company-backends 'company-capf))
+(use-package helm
+  :ensure t
+  :init (helm-mode t))
 
 (use-package org-superstar
-  :init
-  (org-superstar-mode 1)
-(org-superstar-configure-like-org-bullets))
+  :ensure t
+  :after org)
 
-(use-package posframe)
-(use-package lsp-ui
-  :defer t)
-(use-package lsp-mode
-  :init (add-to-list 'company-backends 'company-capf)
-  :config
-  (setq lsp-ui-doc-enable nil)
-  :commands lsp)
+(use-package org-super-agenda
+  :ensure t
+  :init (org-super-agenda-mode)
+  :bind (("C-c a" . org-agenda))
+  )
 
-(use-package magit)
+(defun my-org-mode-hook ()
+  (org-superstar-mode)
+  (org-superstar-configure-like-org-bullets))
+
+(add-hook 'org-mode-hook 'my-org-mode-hook)
